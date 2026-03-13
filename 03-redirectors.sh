e5f9fe6d61d6b991c4db6d23f499c486c17a6960
abhishekdontukurti <dabhishek175@gmail.com>
2026-03-12 23:26:38 -0400

    append feature

--- 03-redirectors.sh
+++ 03-redirectors.sh
@@ -1,2 +1,6 @@
-echo "Hello" 1> /tmp/output.txt
-echo "hello" > /tmp/output1.txt
+#echo "Hello" 1> /tmp/output.txt
+#echo "hello" > /tmp/output1.txt
+
+echo "Line 1" > /tmp/output.txt
+echo "line2" >>/tmp/output.txt
+echo "Line3" >>/tmp/output.txt
