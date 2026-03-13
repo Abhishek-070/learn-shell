8fdd5961cea52159a216bfb36282f91d7fa241f9
abhishekdontukurti <dabhishek175@gmail.com>
2026-03-12 23:29:23 -0400

    append.txt

--- 03-redirectors.sh
+++ 03-redirectors.sh
@@ -1,6 +1,6 @@
 #echo "Hello" 1> /tmp/output.txt
 #echo "hello" > /tmp/output1.txt
 
-echo "Line 1" > /tmp/output.txt
-echo "line2" >> /tmp/output.txt
-echo "Line3" >> /tmp/output.txt
+echo "Line 1" > /tmp/append.txt
+echo "line2" >> /tmp/append.txt
+echo "Line3" >> /tmp/append.txt
