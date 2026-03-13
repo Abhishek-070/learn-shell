92221142618243f6f3965311ebed31134be9a3d1
abhishekdontukurti <dabhishek175@gmail.com>
2026-03-12 23:01:05 -0400

    print in different colours.

--- 01-print.sh
+++ 01-print.sh
@@ -1,2 +1,12 @@
 echo hello world
 echo -e \\e[31mHello world\\e[0m
+echo -e \\e[32mHello world\\e[0m
+echo -e \\e[33mHello world\\e[0m
+echo -e \\e[34mHello world\\e[0m
+echo -e \\e[35mHello world\\e[0m
+
+echo -e \\e[41mHello world\\e[0m
+echo -e \\e[42mHello world\\e[0m
+echo -e \\e[43mHello world\\e[0m
+echo -e \\e[44mHello world\\e[0m
+echo -e \\e[45mHello world\\e[0m
